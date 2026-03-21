993f976e81c42427cc1854c0c686919b54583c1f
Rashmi_kasar <107236092+rkasar1@users.noreply.github.com>
2026-03-21 12:53:37 +0530

    auditing implemented

--- src/main/java/com/enotesApiService/controller/CategoryController.java
+++ src/main/java/com/enotesApiService/controller/CategoryController.java
@@ -27,7 +27,7 @@ public class CategoryController {
 	@Autowired
 	private CategoryService categoryService;
 
-	@PostMapping("/save-category")
+	@PostMapping("save")
 	public ResponseEntity<?> saveCategory(@RequestBody CategoryDto categoryDto) {
 		boolean saveCategory = categoryService.saveCategory(categoryDto);
 		if (saveCategory) {
